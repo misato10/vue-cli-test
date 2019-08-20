3831e5062be00aa44ac495399556ccfe8772054b
misato10 <midori692227@gmail.com>
2019-08-20 23:16:06 +0900

    追加

--- test01/src/App.vue
+++ test01/src/App.vue
@@ -2,16 +2,19 @@
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <example msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import Example from './components/Example.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    HelloWorld,
+    Example
   }
 }
 </script>
---
+++ test01/src/components/Example.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="example">
+    <h1>example</h1> 
+    <p>テスト</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Example',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1 {
+  margin: 40px 0 0;
+  color: #42b983;
+}
+p {
+  color:#666;
+}
+</style>
